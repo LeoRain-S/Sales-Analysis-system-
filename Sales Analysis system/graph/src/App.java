1ef7e2f80488c3df1413248415473cff5028d4c3
LeoRain-S <81185162+LeoRain-S@users.noreply.github.com>
2021-05-12 22:50:52 -0400

    Update App.java

--- Sales Analysis system/graph/src/App.java
+++ Sales Analysis system/graph/src/App.java
@@ -86,25 +86,25 @@ public class App extends Application {
         
         //Displaying the contents of the stage
         stage.show();        
-   }
-   // Main
-   public static void main(String args[]) throws IOException {
-      // The results.csv 
-      readFileAndExportResult();
-      // The bar chart
-      launch(args);
-   }
-   // Author - Eric Chen
-   public static void readFileAndExportResult(){
-      int[] arr = new int[10];
-      int total = 0;
-      double[] freq = new double[10];
+    }
+    // Main
+    public static void main(String args[]) throws IOException {
+        // The results.csv 
+        readFileAndExportResult();
+        // The bar chart
+        launch(args);
+       }
+    // Author - Eric Chen
+    public static void readFileAndExportResult(){
+        int[] arr = new int[10];
+        int total = 0;
+        double[] freq = new double[10];
 
-      try {
-          String line;
-          BufferedReader reader = new BufferedReader(new FileReader(new File("sales.csv")));
-          reader.readLine();
-          while ((line = reader.readLine()) != null) {
+        try {
+            String line;
+            BufferedReader reader = new BufferedReader(new FileReader(new File("sales.csv")));
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
               // System.out.println(line);
               String num = line.split(",")[1];
               int digit = num.charAt(0) - '0';
